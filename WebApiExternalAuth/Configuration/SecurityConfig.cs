5482ead034cc8c511829e1929e4856a07c16461b
juanonsoftware <huanhvhd@gmail.com>
2015-10-14 23:50:31 +0700

    Microsoft account

--- WebApiExternalAuth/Configuration/SecurityConfig.cs
+++ WebApiExternalAuth/Configuration/SecurityConfig.cs
@@ -2,6 +2,7 @@
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Facebook;
 using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.MicrosoftAccount;
 using Owin;
 using WebApiExternalAuth.Models;
 
@@ -22,18 +23,24 @@ namespace WebApiExternalAuth.Configuration
 
             // Configure google authentication
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            {
-                ClientId = "592811454485-lbltg7v9gibiets5pekaasrf0hiltjqg.apps.googleusercontent.com",
-                ClientSecret = "nr9MSTaYquDnojiMd8GsjE2m",
-                Provider = new GoogleCustomAuthenticationProvider(),
-            });
+                {
+                    ClientId = "592811454485-lbltg7v9gibiets5pekaasrf0hiltjqg.apps.googleusercontent.com",
+                    ClientSecret = "nr9MSTaYquDnojiMd8GsjE2m",
+                    Provider = new GoogleCustomAuthenticationProvider(),
+                });
 
             app.UseFacebookAuthentication(new FacebookAuthenticationOptions()
-            {
-                AppId = "118560908500179",
-                AppSecret = "a0650638ae246afa9d974e3a3df45deb",
-                Provider = new FacebookCustomAuthenticationProvider()
-            });
+                {
+                    AppId = "118560908500179",
+                    AppSecret = "a0650638ae246afa9d974e3a3df45deb",
+                    Provider = new FacebookCustomAuthenticationProvider()
+                });
+
+            app.UseMicrosoftAccountAuthentication(new MicrosoftAccountAuthenticationOptions()
+                {
+                    ClientId = "0000000044167B8E",
+                    ClientSecret = "K1BYOzio7sP36np84MDNdd60jXEvQSH-"
+                });
         }
     }
 }
